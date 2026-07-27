600a942cbb367e0690008abd59cbe1dfb230673d
aboy4321 <awboyce630@gmail.com>
2026-07-27 13:02:24 -0400

    beginning code for uniform distribution

--- include/MatrixRandom.h
+++ include/MatrixRandom.h
@@ -4,3 +4,20 @@
 #include <MatrixStats.h>
 #include <random>
 #include <vector>
+
+std::random_device rd;
+std::mt19937 gen(rd());
+
+namespace nerd {
+
+static Matrix<double unif(const double low = 0.0, const double high = 0.0, std::size_t dim) {
+  Matrix<double> res(dim, dim);
+  std::uniform_real_distribution<double> dist(low, high);
+  double random_double = dist(gen);
+  for (std::size_t i = 0; i < dim; ++i) {
+    res[i] = dist(gen);
+  }
+  return res;
+}
+  
+}
